b25a3c104c22b4cff0e03dd8fb332dc9b814d877
Dhiran <97387022+dhiran-dev@users.noreply.github.com>
2023-12-28 00:11:07 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+//Tech-stack used : Vite+ReactJS+ShadcnUi+TailwindCSS
+
+function App() {
+  return (
+    <>
+      <div>Hello</div>
+      <div>
+        <Button className="p-5 m-5 w-full">Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
